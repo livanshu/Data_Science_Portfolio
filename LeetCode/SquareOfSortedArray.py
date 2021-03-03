592ba1c319385e8a3ea67a03e264900297f62466
Livanshu Kashyap <53486980+livanshu@users.noreply.github.com>
2021-03-03 01:01:59 -0500

    Create SquareOfSortedArray.py

---
+++ LeetCode/SquareOfSortedArray.py
@@ -0,0 +1,8 @@
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        
+        for i in range(n):
+            nums[i] = nums[i]*nums[i]
+        nums.sort()
+        return nums
+        
