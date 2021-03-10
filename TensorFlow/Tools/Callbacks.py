f6a21f278a5328a1a0af42d7d57124e2e2b7e4ad
Livanshu Kashyap <53486980+livanshu@users.noreply.github.com>
2021-03-10 04:04:44 -0500

    Update Callbacks.py

--- TensorFlow/Tools/Callbacks.py
+++ TensorFlow/Tools/Callbacks.py
@@ -8,7 +8,7 @@ class myCallback(tf.keras.callbacks.Callback):
       print("\nReached 60% accuracy so cancelling training!")
       self.model.stop_training = True
 
-callbacks = myCallback()
+callbacks = myCallback() ### HERE
 mnist = tf.keras.datasets.fashion_mnist
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 training_images=training_images/255.0
@@ -19,4 +19,4 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks]) ### HERE
